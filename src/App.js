fd5db88010d8c7af0c84d615737365dc43732599
Max Sveshnikov <msveshnikov@gmail.com>
2020-10-03 12:27:38 +0200

    card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { useSpring } from "react-spring";
-import { animated } from "react-spring";
+import { useSpring, animated } from "react-spring";
 
 function App() {
     const [isToggled, setIsToggled] = useState(false);
@@ -13,12 +12,33 @@ function App() {
         fontSize: isToggled ? "2rem" : "3rem",
     });
 
+    const [props, set] = useSpring(() => ({
+        xys: [0, 0, 0.5],
+        config: { mass: 5, tension: 200, friction: 100 },
+    }));
+
+    const perspective = (x, y, s) =>
+        `perspective(500px) 
+   rotateX(${x}deg) 
+   rotateY(${y}deg) 
+   scale(${s})`;
+
+    const calcXY = (x, y) => [-(y - window.innerHeight / 2) / 15, (x - window.innerWidth / 2) / 15, 1.0];
+
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+                {/* <img src={logo} className="App-logo" alt="logo" /> */}
                 <animated.h1 style={fade}>Hello</animated.h1>
                 <button onClick={() => setIsToggled(!isToggled)}>Toggle</button>
+                <br/>
+                <br/>
+                <br/>
+                <animated.div  className='card'
+                    onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calcXY(x, y) })}
+                    onMouseLeave={() => set({ xys: [0, 0, 0.5] })}
+                    style={{ transform: props.xys.interpolate(perspective) }}
+                />
             </header>
         </div>
     );
