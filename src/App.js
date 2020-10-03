51291a0f9c20160ba0c8b7211fa07151e1b24dd6
Max Sveshnikov <msveshnikov@gmail.com>
2020-10-03 11:30:11 +0200

    animate!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useSpring } from "react-spring";
+import { animated } from "react-spring";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isToggled, setIsToggled] = useState(false);
+
+    const fade = useSpring({
+        color: isToggled ? "#fff" : "green",
+        transform: isToggled ? "translate3d(0, 15px, 0)" : "translate3d(0, 15px, 0)",
+        fontSize: isToggled ? "2rem" : "3rem",
+    });
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <animated.h1 style={fade}>Hello</animated.h1>
+                <button onClick={() => setIsToggled(!isToggled)}>Toggle</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
